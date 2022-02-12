faa05712fbff2ee7092f7a778f93633310916a0e
Nick Teaford <nickteaford@Nicks-iMac.local>
2022-02-12 14:21:53 -0800

    styling update preIRO install

--- src/components/FavoritePalettesContainer/FavoritePalettesContainer.jsx
+++ src/components/FavoritePalettesContainer/FavoritePalettesContainer.jsx
@@ -1,28 +1,29 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import SavedPalettesContainer from "../SavedPalettesContainer/SavedPalettesContainer";
 import FavoritePaletteModal from "../FavoritePaletteModal/FavoritePaletteModal";
 
-export default function FavoritePalettesContainer ({favoritePalettes, toggleStale}) {
-    const [showFPM, setShowFPM] = useState(false);
-    const [paletteSelected, setPaletteSelected] = useState('');
+export default function FavoritePalettesContainer({ favoritePalettes, toggleStale }) {
+  const [showFPM, setShowFPM] = useState(false);
+  const [paletteSelected, setPaletteSelected] = useState('');
 
-    function showFavoritePaletteModal(palette) {
-        setShowFPM(true);
-        setPaletteSelected(palette);
-      };
-    
-      function hideFavoritePaletteModal() {
-        setShowFPM(false);
-        setPaletteSelected("");
-      };
+  function showFavoritePaletteModal(palette) {
+    setShowFPM(true);
+    setPaletteSelected(palette);
+  };
 
-    return (
-        <div>
-           {favoritePalettes.map((palette, idx) => <SavedPalettesContainer palette={palette} key = {idx} showFavoritePaletteModal={showFavoritePaletteModal} /> )}
-           
-          
-           <FavoritePaletteModal palette={paletteSelected} hideFavoritePaletteModal={hideFavoritePaletteModal} showFPM={showFPM} toggleStale={toggleStale}  />
+  function hideFavoritePaletteModal() {
+    setShowFPM(false);
+    setPaletteSelected("");
+  };
 
-        </div>
-    )
+  return (
+    <div>
+
+      {favoritePalettes.map((palette, idx) => <SavedPalettesContainer palette={palette} key={idx} showFavoritePaletteModal={showFavoritePaletteModal} />)}
+
+
+      <FavoritePaletteModal palette={paletteSelected} hideFavoritePaletteModal={hideFavoritePaletteModal} showFPM={showFPM} toggleStale={toggleStale} />
+
+    </div>
+  )
 }
